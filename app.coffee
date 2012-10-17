0c4baf0b4e686e15dc643aeba6a9a947c241739a
Eugene Mirotin <emirotin@gmail.com>
2012-10-17 17:05:22 +0300

    socket.io

--- app.coffee
+++ app.coffee
@@ -1,7 +1,11 @@
-express = require 'express'
-
 path = require 'path'
+
+express = require 'express'
+socket_io = require('socket.io')
+
 app = express()
+server = require('http').createServer(app)
+io = socket_io.listen server
 
 app_root = __dirname
 static_path = path.join app_root, 'static'
@@ -14,9 +18,25 @@ app
     .use(express.methodOverride())
     .use(app.router)
     .use(express.static static_path)
-    #.use(connect_assets(src: 'static'))
 
 app.get '/', (req, res) ->
     res.render 'index'
 
-app.listen process.env.PORT or 5000
+clients = {}
+client_id = 0
+
+io.sockets.on 'connection', (socket) ->
+    client_id += 1
+    clients[client_id] = socket
+    socket.emit 'login info', { id: client_id }
+    socket.on 'line create', (data) ->
+        console.log data
+        socket.broadcast.emit 'line create', data
+    socket.on 'line update', (data) ->
+        socket.broadcast.emit 'line update', data
+    socket.on 'line end', (data) ->
+        socket.broadcast.emit 'line end', data
+
+port = process.env.PORT or 5000
+
+server.listen port
--- static/javascripts/draw.js
+++ static/javascripts/draw.js
@@ -1,45 +1,109 @@
 $(function() {
-    var stage = new Kinetic.Stage({
+    var socket = io.connect(document.location.host),
+        stage = new Kinetic.Stage({
             container: "drawing-stage",
             width: 700,
             height: 400,
             listening: true
         }),
         layer = new Kinetic.Layer(),
-        currentLinePoints,
-        currentLine,
-        drawing = false;
+        drawing = false,
+        client_id,
+        my_line_id = 0,
+        current_line_id,
+        lines = {};
+
     stage.add(layer);
+
+    function create_line(data) {
+        var id = data.id, points = data.points,
+            res;
+        if (id in lines) {
+            return;
+        }
+        res = lines[id] = {
+            line: null,
+            points: points
+        };
+        draw_line(res);
+        return res;
+    }
+
+    function draw_line(line_data) {
+        if (line_data.line) {
+            line_data.line.remove();
+        }
+        line_data.line = new Kinetic.Line({
+            points: line_data.points,
+            stroke: "red",
+            strokeWidth: 15,
+            lineCap: "round",
+            lineJoin: "round"
+        });
+        layer.add(line_data.line);
+        layer.draw();
+    }
+
+    function update_line(data) {
+        var id = data.id, points = data.points,
+            line_data = lines[id];
+        if (!line_data) {
+            line_data = create_line(id, points);
+        }
+        line_data.points = points;
+        draw_line(line_data);
+    }
+
+    function end_line(data) {
+        var id = data.id;
+        delete lines[id];
+    }
 
     $('#drawing-stage .kineticjs-content').on({
         'mousedown touchstart': function (evt) {
             drawing = true;
-            var coords = stage.getUserPosition(evt);
-            currentLinePoints = [coords.x, coords.y];
+            my_line_id += 1;
+            current_line_id = client_id + ':' + my_line_id;
+            var coords = stage.getUserPosition(evt),
+                points = [coords.x, coords.y],
+                data = {id: current_line_id, points: points};
+            create_line(data);
+            socket.emit('line create', data);
         },
         'mousemove touchmove': function (evt) {
             if (!drawing) {
                 return;
             }
-            var coords = stage.getUserPosition(evt);
-            currentLinePoints.push(coords.x, coords.y);
-            if (currentLine) {
-                currentLine.remove();
-            }
-            currentLine = new Kinetic.Line({
-                points: currentLinePoints,
-                stroke: "red",
-                strokeWidth: 15,
-                lineCap: "round",
-                lineJoin: "round"
-            });
-            layer.add(currentLine);
-            layer.draw();
+            var coords = stage.getUserPosition(evt),
+                points = lines[current_line_id].points,
+                data;
+            points.push(coords.x);
+            points.push(coords.y);
+            data = { id: current_line_id, points: points };
+            update_line(data);
+            socket.emit('line update', data);
         },
         'mouseup touchend mouseout': function () {
+            var data = { id: current_line_id };
             drawing = false;
-            currentLine = null;
-            currentLinePoints = null;
+            end_line(data);
+            socket.emit('line end', data);
+            current_line_id = null;
         }
     });
+
+    socket.on('login info', function(data) {
+       client_id = data.id;
+    });
+
+    socket.on('line create', function(data) {
+        create_line(data);
+    });
+    socket.on('line update', function(data) {
+        update_line(data);
+    });
+    socket.on('line end', function(data) {
+        end_line(data);
+    });
+
 });
